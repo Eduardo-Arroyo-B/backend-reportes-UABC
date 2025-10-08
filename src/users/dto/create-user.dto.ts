e532e95d3b1834b19e7293ad022c02bb4b867333
Eduardo Arroyo <aeduardoiran@gmail.com>
2025-10-07 19:53:10 -0700

    feat: Logica para crear usuarios con validaciones de campos

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,26 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, MinLength, IsEnum } from 'class-validator';
+// @ts-ignore
+import { Role } from '@prisma/client';
+
+/**
+ * Data Transfer Object para crear el usuario con validaciones.
+ */
+
+export class CreateUserDto {
+  @IsNotEmpty({ message: 'El nombre no debe estar vacío' })
+  name: string;
+
+  @IsEmail({}, { message: 'El correo electrónico no es válido' })
+  @IsNotEmpty({ message: 'El correo electrónico no debe estar vacío' })
+  email: string;
+
+  @MinLength(6, { message: 'La contraseña debe tener al menos 6 caracteres' })
+  @IsNotEmpty({ message: 'La contraseña no debe estar vacía' })
+  password: string;
+
+  @IsNotEmpty({ message: 'La confirmación de la contraseña no debe estar vacía' })
+  password_confirm: string;
+
+  @IsEnum(Role, { message: 'El rol no es válido' })
+  role: Role;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,84 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
+import prisma from '../../prisma/PrismaClient';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  // Servicio para crear un nuevo usuario
+  async create(createUserDto: CreateUserDto) {
+
+    try {
+      const { password_confirm, password, ...userData } = createUserDto;
+
+      const user = await prisma.user.findUnique({
+        where: {
+          email: userData.email,
+        },
+      });
+
+      if (user) {
+        throw new HttpException('El correo electrónico ya está en uso', HttpStatus.BAD_REQUEST);
+      }
+
+      if (password !== password_confirm) {
+        throw new HttpException('Las contraseñas no coinciden', HttpStatus.BAD_REQUEST);
+      }
+
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(password, salt);
+
+      const createUser = await prisma.user.create({
+        data: {
+          ...userData,
+          password: hashedPassword,
+        },
+      })
+
+      if (!createUser) {
+        throw new HttpException('User not created', HttpStatus.BAD_REQUEST);
+      }
+
+      return {
+        status: HttpStatus.CREATED,
+        message: 'Usuario creado exitosamente',
+        data: createUser,
+      }
+    } catch (error) {
+        throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
-  findAll() {
-    return `This action returns all users`;
+  // Servicio para obtener todos los usuarios
+  async findAll() {
+    try {
+      const users = await prisma.user.findMany();
+
+      if (!users) {
+        throw new HttpException('No users found', HttpStatus.NOT_FOUND);
+      }
+
+      return {
+        status: HttpStatus.OK,
+        message: 'Usuarios encontrados exitosamente',
+        data: users,
+      }
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
-  findOne(id: number) {
+  // Servicio para obtener un usuario por su ID
+  async findOne(id: number) {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+  async update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} user`;
   }
 }
